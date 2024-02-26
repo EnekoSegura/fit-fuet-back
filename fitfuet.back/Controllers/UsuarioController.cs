8047481f572f1d23a44f9a69ec7c127526b1079f
Eneko <dam3.eneko.segura@gmail.com>
2024-02-26 11:59:02 +0100

    enviar_email: enviar email de prueba desde el backend + añadir strings de conexion

--- fitfuet.back/Controllers/UsuarioController.cs
+++ fitfuet.back/Controllers/UsuarioController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace fitfuet.back.Controllers
@@ -63,6 +66,36 @@ namespace fitfuet.back.Controllers
         public ActionResult<string> prueba()
         {
             return Ok("weka");
+        }
+
+        private static string email = "fitfuet@gmail.com";
+        private static string destino = "dam3.erlantzgsc@gmail.com";
+        //private static string contra = "Fitfuet2024";
+        //private static string destino = "dam3.erlantzgsc@gmail.com";
+
+        [HttpGet("enviar")]
+        public ActionResult<string> enviarMail()
+        {
+            var message = new MailMessage()
+            {
+                From = new MailAddress(email),
+                Subject = "Prueba",
+                IsBodyHtml = true,
+                Body = "Cuerpo de la prueba",
+            };
+
+            message.To.Add(new MailAddress(destino));
+
+            var smtp = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(email, _config["ClaveAplicacion"]),
+                EnableSsl = true,
+            };
+
+            smtp.Send(message);
+
+            return Ok("Correcto");
         }
     }
 }
