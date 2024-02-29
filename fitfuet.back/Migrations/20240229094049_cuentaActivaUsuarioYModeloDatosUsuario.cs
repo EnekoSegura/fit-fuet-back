3cfa57481afefca1949edc71ae927b31dafed220
Eneko <dam3.eneko.segura@gmail.com>
2024-02-29 12:35:53 +0100

    Modelo DatosUsuario

---
+++ fitfuet.back/Migrations/20240229094049_cuentaActivaUsuarioYModeloDatosUsuario.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace fitfuet.back.Migrations
+{
+    public partial class cuentaActivaUsuarioYModeloDatosUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CuentaActiva",
+                table: "Usuario",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "DatosUsuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdUsuario = table.Column<int>(nullable: false),
+                    Peso = table.Column<float>(nullable: false),
+                    Altura = table.Column<float>(nullable: false),
+                    FechaRegistro = table.Column<DateTime>(nullable: false),
+                    RegistroActivo = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DatosUsuario", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DatosUsuario_Usuario_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DatosUsuario_IdUsuario",
+                table: "DatosUsuario",
+                column: "IdUsuario");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DatosUsuario");
+
+            migrationBuilder.DropColumn(
+                name: "CuentaActiva",
+                table: "Usuario");
+        }
+    }
+}
---
+++ fitfuet.back/Models/DatosUsuario.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace fitfuet.back.Models
+{
+    public class DatosUsuario: EntidadBase
+    {
+        [Required]
+        public int IdUsuario { get; set; }
+        [ForeignKey("IdUsuario")]
+        public Usuario Usuario { get; set; }
+        [Required]
+        public float Peso { get; set; } //peso en kg
+        [Required]
+        public float Altura { get; set; } //altura en cms
+        [Required]
+        public DateTime FechaRegistro { get; set; }
+        [Required]
+        public int RegistroActivo { get; set; } //0 activa, 1 inactiva
+    }
+}
